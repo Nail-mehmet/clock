8bb23dc0082288c4eaeb3b65bb92c78fc0962183
Nail-mehmet <mehmetnailkalkan@gmail.com>
2024-10-06 12:08:05 +0300

    ilan yükleme backend neredeyse bitti

--- lib/Screens/Profile/sell_watch_page.dart
+++ lib/Screens/Profile/sell_watch_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SellWatchPage extends StatefulWidget {
   @override
@@ -8,13 +12,22 @@ class SellWatchPage extends StatefulWidget {
 }
 
 class _SellWatchPageState extends State<SellWatchPage> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? _imageFiles = [];
+  List<String> _uploadedFileURLs = [];
+  bool _isLoading = false;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
   final _formKey = GlobalKey<FormState>();
   final _mechanismTypeController = TextEditingController();
   final _yearController = TextEditingController();
   final _conditionController = TextEditingController();
   final _descriptionController = TextEditingController();
-
   String? _selectedBrand;
+  String? _selectedType;
+  String? _condition;
+  int _remainingChars = 150;
+
   List<String> watchBrands = [
     'Rolex',
     'Patek Philippe',
@@ -37,35 +50,92 @@ class _SellWatchPageState extends State<SellWatchPage> {
     'Chopard',
     'Vacheron Constantin',
   ];
+  List<String> systemType = [
+    'pilli',
+    'Mekanik',
+    'Solar',
+    'Kurmalı',
+  ];
+  List<String> condition = [
+    'sıfır',
+    'kulanılmış',
+    'ikinci el',
+    'yenilnmiş',
+  ];
 
-  void _submit() async {
+  Future<void> _pickImages() async {
+    final List<XFile>? selectedImages = await _picker.pickMultiImage(imageQuality: 80);
+    if (selectedImages != null && selectedImages.length <= 5) {
+      setState(() {
+        _imageFiles!.addAll(selectedImages);
+      });
+    } else if (selectedImages != null && selectedImages.length > 5) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('En fazla 5 fotoğraf seçebilirsiniz!'),
+      ));
+    }
+  }
+
+  Future<void> _uploadImages() async {
+    for (var imageFile in _imageFiles!) {
+      String fileName = path.basename(imageFile.path);
+      Reference storageRef = _storage.ref().child('images/$fileName');
+      UploadTask uploadTask = storageRef.putFile(File(imageFile.path));
+
+      TaskSnapshot snapshot = await uploadTask;
+      String downloadURL = await snapshot.ref.getDownloadURL();
+      _uploadedFileURLs.add(downloadURL);
+    }
+  }
+
+  Future<void> _saveToFirestore() async {
     if (_formKey.currentState!.validate()) {
       User? currentUser = FirebaseAuth.instance.currentUser;
 
-      // Firestore'a kaydet
       await FirebaseFirestore.instance.collection('listings').add({
         'userId': currentUser!.uid,
         'brand': _selectedBrand,
-        'mechanismType': _mechanismTypeController.text,
+        'mechanismType': _selectedType,
         'year': _yearController.text,
-        'condition': _conditionController.text,
+        'condition': _condition,
         'description': _descriptionController.text,
         'timestamp': FieldValue.serverTimestamp(),
+        "images": _uploadedFileURLs
       });
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Saat bilgileri başarıyla kaydedildi!')),
       );
 
-      // Formu temizle
       setState(() {
         _selectedBrand = null;
+        _selectedType = null;
+        _condition = null;
       });
-      _mechanismTypeController.clear();
+      //_mechanismTypeController.clear();
       _yearController.clear();
-      _conditionController.clear();
+      //_conditionController.clear();
       _descriptionController.clear();
     }
+  }
+
+  Future<void> _saveAllData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await _uploadImages();
+    await _saveToFirestore();
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _removeImage(int index) {
+    setState(() {
+      _imageFiles!.removeAt(index);
+    });
   }
 
   @override
@@ -86,10 +156,50 @@ class _SellWatchPageState extends State<SellWatchPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Saat Markası',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    GestureDetector(
+                      onTap: _pickImages,
+                      child: DottedBorderButton(),
                     ),
+                    SizedBox(height: 20),
+
+                    // Seçilen Fotoğrafların Önizlemesi
+                    if (_imageFiles!.isNotEmpty)
+                      SizedBox(
+                        height: 100,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _imageFiles!.length,
+                          itemBuilder: (context, index) {
+                            return Stack(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image.file(
+                                      File(_imageFiles![index].path),
+                                      width: 100,
+                                      height: 100,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 0,
+                                  right: 0,
+                                  child: IconButton(
+                                    icon: Icon(Icons.delete, color: Colors.red),
+                                    onPressed: () => _removeImage(index),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      )
+                    else
+                      Text('Henüz fotoğraf seçilmedi', style: TextStyle(fontSize: 16)),
+                    SizedBox(height: 20),
                     Container(
                       padding: const EdgeInsets.symmetric(horizontal: 12.0),
                       decoration: BoxDecoration(
@@ -100,7 +210,7 @@ class _SellWatchPageState extends State<SellWatchPage> {
                         child: DropdownButton<String>(
                           value: _selectedBrand,
                           isExpanded: true,
-                          hint: Text('Seçin'),
+                          hint: Text('Saatin Markası'),
                           items: watchBrands.map((String brand) {
                             return DropdownMenuItem<String>(
                               value: brand,
@@ -121,39 +231,34 @@ class _SellWatchPageState extends State<SellWatchPage> {
                   ],
                 ),
               ),
-
-              // Mekanizma Tipi
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
-                    labelText: 'Mekanizma Tipi',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.grey, width: 1),
-                    ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: _selectedType,
+                    isExpanded: true,
+                    hint: Text('Mekanizma'),
+                    items: systemType.map((String type) {
+                      return DropdownMenuItem<String>(
+                        value: type,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(type),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedType = newValue;
+                      });
+                    },
                   ),
-                  items: ['Otomatik', 'Pilli', 'Kurmalı'].map((String type) {
-                    return DropdownMenuItem<String>(
-                      value: type,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0), // Padding eklendi
-                        child: Text(type),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    _mechanismTypeController.text = value ?? '';
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Lütfen mekanizma tipini seçin';
-                    }
-                    return null;
-                  },
                 ),
               ),
-
               // Çıkış Yılı
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -177,50 +282,53 @@ class _SellWatchPageState extends State<SellWatchPage> {
               ),
 
               // Kondisyon
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
-                    labelText: 'Kondisyon',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.grey, width: 1),
-                    ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: _condition,
+                    isExpanded: true,
+                    hint: Text('Kondisyon'),
+                    items: condition.map((String condition) {
+                      return DropdownMenuItem<String>(
+                        value: condition,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(condition),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _condition = newValue;
+                      });
+                    },
                   ),
-                  items: ['Yeni', 'İkinci El', 'Kullanılmış'].map((String condition) {
-                    return DropdownMenuItem<String>(
-                      value: condition,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(condition),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    _conditionController.text = value ?? '';
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Lütfen kondisyona göre bir seçim yapın';
-                    }
-                    return null;
-                  },
                 ),
               ),
 
-              // Açıklama (Çok satırlı)
+              // Açıklama
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: TextFormField(
                   controller: _descriptionController,
                   decoration: InputDecoration(
                     labelText: 'Açıklama',
+                    hintText: 'Açıklama girin', // Hint text sol üstte görünmeye devam edecek
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                       borderSide: BorderSide(color: Colors.grey, width: 1),
                     ),
+                    alignLabelWithHint: true, // HintText'in yukarıda sabitlenmesini sağlar
                   ),
-                  maxLines: 3, // Açıklama alanı için 3 satır
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null, // Alt satıra geçme özelliği korunur
+                  minLines: 3, // İlk görünümde minimum 3 satır gösterilir
+                  maxLength: 150, // 150 karakter sınırı
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Lütfen açıklama girin';
@@ -228,12 +336,15 @@ class _SellWatchPageState extends State<SellWatchPage> {
                     return null;
                   },
                 ),
+
               ),
 
+              // Kaydet Butonu
               SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: _submit,
+              _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : ElevatedButton(
+                onPressed: _saveAllData,
                 child: Text('Kaydet'),
               ),
             ],
@@ -243,3 +354,33 @@ class _SellWatchPageState extends State<SellWatchPage> {
     );
   }
 }
+
+// Kesik Çizgili Buton
+class DottedBorderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.blueAccent, style: BorderStyle.solid, width: 1),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.photo_library, size: 50, color: Colors.blue),
+            SizedBox(height: 10),
+            Text(
+              'Fotoğrafları Yükle',
+              style: TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
